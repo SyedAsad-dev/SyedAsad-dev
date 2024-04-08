66aa490354f6ee85aa8867c7a1f406737e076395
TAyes <syedasadali786786@gmail.com>
2024-04-08 22:16:22 +0400

    complete

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"https://mr-muhammad-rehan.github.io/mr-muhammad-rehan/",
+  base:"https://SyedAsad-dev.github.io/SyedAsad-dev/",
   plugins: [react()],
 })
