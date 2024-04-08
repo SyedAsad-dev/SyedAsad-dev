32fb3bc46fe9ab948a86880aba371e4988573bf2
TAyes <syedasadali786786@gmail.com>
2024-04-08 21:45:03 +0400

    first init

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base:"https://mr-muhammad-rehan.github.io/mr-muhammad-rehan/",
+  plugins: [react()],
+})
